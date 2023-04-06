c70c666974998dbf3aaea81c52404487278a8c1c
Karan <karanrao81441@outlook.com>
2023-04-06 09:41:26 +0530

    Adding Solution File for Day 02 of 30 day of JavaScript

---
+++ Day002/example.js
@@ -0,0 +1,37 @@
+// const num = Math.floor(Math.random() * 11)
+// console.log(num)
+
+// console.log('I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?') // line break
+// console.log('Days\tTopics\tExercises')
+// console.log('Day 1\t3\t5')
+// console.log('Day 2\t3\t5')
+// console.log('Day 3\t3\t5')
+// console.log('Day 4\t3\t5')
+// console.log('This is a backslash  symbol (\\)') // To write a backslash
+// console.log('In every programming language it starts with \"Hello, World!\"')
+// console.log("In every programming language it starts with \'Hello, World!\'")
+// console.log('The saying \'Seeing is Believing\' isn\'t correct in 2020')
+
+// console.log(`The sum of 2 and 3 is 5`)              // statically writing the data
+// let a = 2
+// let b = 3
+// console.log(`The sum of ${a} and ${b} is ${a + b}`) // injecting the data dynamically
+
+// #1
+let challenge = "30 Day of JavaScript"
+
+// #2 Print the value inside string challenge
+console.log(challenge)
+
+// #3 Print length
+console.log(challenge.length)
+
+// #4 Change the string characters into upper case
+console.log(challenge.toUpperCase)
+
+// #5 Change the string characters into lower case
+console.log(challenge.toLowerCase)
+
+// #6 cut the first word of the string
+console.log(challenge.slice(0,2)) // Using slice()
+console.log(challenge.substring(0,2)) //Using substring()
---
+++ Day002/level1.js
@@ -0,0 +1,18 @@
+// #1
+let challenge = "30 Day of JavaScript"
+
+// #2 Print the value inside string challenge
+console.log(challenge)
+
+// #3 Print length
+console.log(challenge.length)
+
+// #4 Change the string characters into upper case
+console.log(challenge.toUpperCase)
+
+// #5 Change the string characters into lower case
+console.log(challenge.toLowerCase)
+
+// #6 cut the first word of the string
+console.log(challenge.slice(0,2)) // Using slice()
+console.log(challenge.substring(0,2)) //Using substring()
