ddb489ff06b63548def6969f3f25401c357d334d
Karan <karanrao81441@outlook.com>
2023-04-09 21:32:14 +0530

    Added Solution for LEVEL 3; Completed 2/3 questions

---
+++ Day002/level3.js
@@ -0,0 +1,25 @@
+// #1 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+const sentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+const pattern = /love/gi;
+console.log(sentence.match(pattern)); 
+
+// #2 Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+const sentence2 = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence2.match(/because/g).length);
+
+// #3 Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+/**
+ *  const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;.
+ *  The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. 
+ * ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. 
+ * %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? 
+ * %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt 
+ * of &love& of tea&ching'
+ */
+
+const sentence3 = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+
+const clearedSentence = sentence3.replace(/[\%\$\#\@\&\;\!\?]/gi, '') // To clear all the symbols
+console.log(clearedSentence);
+const frequentWord = clearedSentence.split(' ');
+console.log(frequentWord);
