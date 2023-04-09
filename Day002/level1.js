db4e8a913f2a1e68ee1c74eef92278be3e086526
Karan <karanrao81441@outlook.com>
2023-04-09 13:50:31 +0530

    Added Solutions from #17 to #25 questions of LEVEL 1

--- Day002/level1.js
+++ Day002/level1.js
@@ -47,3 +47,33 @@ console.log(challenge.indexOf());
 
 // #16 Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
 console.log(challenge.lastIndexOf());
+
+// #17 Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let sentence = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.indexOf('because'));
+
+// #18 Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.lastIndexOf('because'));
+
+// #19 Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log(sentence.search('becasue'));
+
+// #20 Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+let challenge2 = ' 30 Days Of JavaScript '
+console.log(challenge2.trim(' '));
+
+// #21 Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.startsWith('30'));
+
+// #22 Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+console.log(challenge.endsWith('JavaScript'));
+
+// #23 Use match() method to find all the a’s in 30 Days Of JavaScript
+console.log(challenge.match('of'));
+
+//  #24 Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+let sentence2 = '30 Days of'
+console.log(sentence2.concat(' JavaScript'));
+
+// #25 Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log(challenge.repeat(2)); // Prints Output for 2 times
