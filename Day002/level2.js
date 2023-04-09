aa3b61af5fe2bf710e70e671731ced9ba929982b
Karan <karanrao81441@outlook.com>
2023-04-09 16:39:32 +0530

    Added Solution file for LEVEL 2 questions; Completed 1st and 2and questions

---
+++ Day002/level2.js
@@ -0,0 +1,16 @@
+// #1 Using console.log() print out the following statement:
+/** 
+ * The quote 'There is no exercise better for the heart than 
+ * reaching down and lifting people up.' by John Holmes teaches us to help one another.
+*/
+
+console.log(' \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another');
+
+// #2 Using console.log() print out the following quote by Mother Teresa:
+/**
+ * "Love is not patronizing and charity isn't about pity, it is about love. 
+ * Charity and love are the same -- with charity you give love, 
+ * so don't just give money but reach out your hand instead."
+ */
+
+console.log(` \"Love is not patronizing and charity isn\'t about pity, it is about love.Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"  `);
