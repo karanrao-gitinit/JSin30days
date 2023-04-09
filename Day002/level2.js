62707c02dfe7ee450a21acfbe0b4f9cd3670607f
Karan <karanrao81441@outlook.com>
2023-04-09 17:37:30 +0530

    Added solution of Level 2 questions from #3 to #9

--- Day002/level2.js
+++ Day002/level2.js
@@ -14,3 +14,27 @@ console.log(' \'There is no exercise better for the heart than reaching down and
  */
 
 console.log(` \"Love is not patronizing and charity isn\'t about pity, it is about love.Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"  `);
+
+// #3 Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+console.log(typeof '10' == typeof 10)
+
+// #4 Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+console.log(parseFloat(9.8) === 10);
+
+// #5 Check if 'on' is found in both python and jargon
+let word1 = 'python', word2 = 'jargon'
+let isPresent = (word1.includes('on') == word2.includes('on'));
+console.log(isPresent);
+
+// #6 I hope this course is not full of jargon. Check if jargon is in the sentence.
+let sentence = 'I hope this course is not full of jargon.'
+console.log(sentence.match('jargon'));
+
+// #7 Generate a random number between 0 and 100 inclusively.
+console.log(Math.floor(Math.random() * 101))
+
+// #8 Write a program in Javascript to Generate a random number between 50 and 100 inclusively.
+console.log(Math.floor(Math.random() * 51) + 50);
+
+// #9 Generate a random number between 0 and 255 inclusively.
+console.log(Math.floor(Math.random() * 256));
