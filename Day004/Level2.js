9787e392855747205c61759f2afe6e3e0b025553
Karan <karanrao81441@outlook.com>
2023-04-22 23:33:52 +0530

    Added solution for Q.No. #2

--- Day004/Level2.js
+++ Day004/Level2.js
@@ -28,3 +28,30 @@ switch (true) {
     default:
         console.log(`Entered Value is more than 100 or Not a Number`);
 }
+
+/**
+ #2 Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+
+    September, October or November, the season is Autumn.
+    December, January or February, the season is Winter.
+    March, April or May, the season is Spring
+    June, July or August, the season is Summer
+*/
+
+let month = window.prompt(`Enter Month`)
+
+switch (true) {
+    case month == `September` || month == `October` || month == `November`:
+        console.log(`Autumn`);
+        break;
+    case month == `December` || month == `January` || month == `February`:
+        console.log(`Winter`);
+        break;    
+    case month == `March` || month == `April` || month == `May`:
+        console.log(`Spring`);
+        break;
+    case month == month == `June` || month == `July` || month == `August`:
+        console.log(`Summer`);
+    default:
+        console.log(`Your Input is not a Month`);
+}
