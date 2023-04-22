9d708aa2cd17eb3fade07892f0f14bce7fd8b608
Karan <karanrao81441@outlook.com>
2023-04-22 18:01:08 +0530

    Added solution for Q.No. #2

--- Day004/Level1.js
+++ Day004/Level1.js
@@ -18,3 +18,22 @@ if (age >= legalAge) {
 } else {
     console.log(`Wait for ${legalAge - age} years`);
 }
+
+/**
+ Compare the values of myAge and yourAge using if … else. Based on the comparison 
+ and log the result to console stating who is older (me or you).
+ Use prompt(“Enter your age:”) to get the age as input.
+
+    Enter your age: 30
+    You are 5 years older than me.
+*/
+
+let yourAge = window.prompt('Enter your age: ')
+const myAge = 23
+
+
+if (myAge > yourAge) {
+    console.log(`I am ${myAge - yourAge} older than you`);
+} else {
+    console.log(`Your are ${myAge - yourAge} older than me`);
+}
