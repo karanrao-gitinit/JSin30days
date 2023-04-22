e476d3ca761b0874812971b930728d7ebfc40937
Karan <karanrao81441@outlook.com>
2023-04-22 18:24:17 +0530

    Added solution for Q.No. #4

--- Day004/Level1.js
+++ Day004/Level1.js
@@ -1,5 +1,5 @@
 /**
-  Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:
+  #1 Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:
  'You are old enough to drive' but if not 18 give another feedback stating to wait for the
   number of years he needs to turn 18.
 
@@ -20,7 +20,7 @@ if (age >= legalAge) {
 }
 
 /**
- Compare the values of myAge and yourAge using if … else. Based on the comparison 
+ #2 Compare the values of myAge and yourAge using if … else. Based on the comparison 
  and log the result to console stating who is older (me or you).
  Use prompt(“Enter your age:”) to get the age as input.
 
@@ -39,7 +39,8 @@ if (myAge > yourAge) {
 }
 
 /**
-    If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+    #3 If a is greater than b return 'a is greater than b' else 'a is less than b'. 
+    Try to implement it in to ways
 
     - using if else
     - ternary operator.
@@ -63,3 +64,22 @@ if (a > b) {
 
 let result = a > b ? `${a} is greater than ${b}` : `${b} is greater than ${a}`
 console.log(result); 
+
+/**
+    #4 Even numbers are divisible by 2 and the remainder is zero.
+    How do you check, if a number is even or not using JavaScript?
+
+    Enter a number: 2
+    2 is an even number
+
+    Enter a number: 9
+    9 is is an odd number.
+*/
+
+let num = window.prompt('Enter a number: ')
+
+if (num / 2 == 0) {
+    console.log(`${num} is an even number`);
+} else {
+    console.log(`${num} is an odd number`);
+}
