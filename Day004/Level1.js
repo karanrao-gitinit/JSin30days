7c4dcf25a5406f3933017e5f84bd35711e9446da
Karan <karanrao81441@outlook.com>
2023-04-22 17:50:45 +0530

    Added solution for Q.No. #1

---
+++ Day004/Level1.js
@@ -0,0 +1,20 @@
+/**
+  Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:
+ 'You are old enough to drive' but if not 18 give another feedback stating to wait for the
+  number of years he needs to turn 18.
+
+    Enter your age: 30
+    You are old enough to drive.
+
+    Enter your age:15
+    You are left with 3 years to drive.
+*/
+
+let age = window.prompt('Enter your age:')
+const legalAge = 18;
+
+if (age >= legalAge) {
+    console.log(`You are old enough to drive`);
+} else {
+    console.log(`Wait for ${legalAge - age} years`);
+}
