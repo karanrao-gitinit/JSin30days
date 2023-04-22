aa965356219bbb239600522721948475e2680135
Karan <karanrao81441@outlook.com>
2023-04-22 23:06:36 +0530

    Added correct Operator to perform Division

--- Day004/Level1.js
+++ Day004/Level1.js
@@ -78,7 +78,7 @@ console.log(result);
 
 let num = window.prompt('Enter a number: ')
 
-if (num / 2 == 0) {
+if (num % 2 == 0) {
     console.log(`${num} is an even number`);
 } else {
     console.log(`${num} is an odd number`);
