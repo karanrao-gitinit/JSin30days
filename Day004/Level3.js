2c02f3566ee68d824405fcd35d095521d5ff4650
Karan <karanrao81441@outlook.com>
2023-04-23 17:48:25 +0530

    Added solution for Q.No. #2

--- Day004/Level3.js
+++ Day004/Level3.js
@@ -24,12 +24,12 @@
     (i.e. the code will wait for the user to enter input before continuing).
     The user's response is stored in the month variable,
     which can then be used to print a message to the console.
-*/   
+*/
 const prompt = require('prompt-sync')();
 
 // Using trim function to remove any leading or trailing spaces
 // Using toLowerCase() to convert user input into lower case and make it uniform
-const month = prompt('Enter the month: ').trim().toLowerCase(); 
+const month = prompt('Enter the month: ').trim().toLowerCase();
 
 let days;
 
@@ -40,7 +40,7 @@ switch (month) {
     case `july`:
     case `august`:
     case `october`:
-    case `december`:   
+    case `december`:
         days = 31;
         console.log(`${month} has ${days}`);
         break;
@@ -51,14 +51,42 @@ switch (month) {
     case `november`:
         days = 30;
         console.log(`${month} has ${days}`);
-        break; 
-        
+        break;
+
     case `february`:
         days = 28;
         console.log(`${month} has ${days}`);
         break;
 
     default:
-        console.log(`INVALID INPUT: Please enter a valid month`); 
+        console.log(`INVALID INPUT: Please enter a valid month`);
         break;
 }
+
+// #2 Write a program which tells the number of days in a month, now consider leap year.  -
+
+let year = prompt(`Enter the Year: `);
+let inputMonth = prompt(`Enter the Month Number from 1 to 12: `);
+
+//Convert the user input string into number
+year = Number(year);
+inputMonth = Number(inputMonth);
+
+// Check if its a leap year or not 
+const isLeapYear = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+? console.log(`${year} is a Leap year`)
+: console.log(`${year} is not a Leap year`)
+
+let numberOfDays;
+
+if (inputMonth == 2) {
+    numberOfDays = isLeapYear ? 29 : 28;
+} else if (inputMonth == 4 || inputMonth == 6 || inputMonth == 9 || inputMonth == 11) {
+    numberOfDays = 30;
+} else if (inputMonth == 1 || inputMonth == 3 || inputMonth == 5 || inputMonth == 7 || inputMonth == 8 || inputMonth == 10 || inputMonth == 12) {
+    numberOfDays = 31;
+} else {
+    console.log(`INVALID INPUT: Enter valid month from 1 to 12`);
+}
+
+console.log(`There are ${numberOfDays} in ${inputMonth}/${year}`);
