abf7a4004ea944e01e8f7611abd6fb9f427bc466
Karan <karanrao81441@outlook.com>
2023-05-05 06:34:24 +0530

    Created folder for LEVEL 2, Created countries.js file, Created web_tech.js file, Created main.js file to access both countries.js and web_tech.js files

---
+++ Day005/Level 2/countries.js
@@ -0,0 +1,12 @@
+const countries = [
+    `India`,
+    `Pakistan`,
+    `Nepal`,
+    `Bhutan`,
+    `China`,
+    `Bangladesh`,
+    `SriLanka`,
+    `Maldives`,
+    `Afganistan`,
+    `Singapore`
+]
---
+++ Day005/Level 2/web_tech.js
@@ -0,0 +1,17 @@
+const web_techs = [
+    `HTML`,
+    `CSS`,
+    `Javascript`,
+    `Java`,
+    `C`,
+    `C++`,
+    `C#`,
+    `.NET`,
+    `Apex`,
+    `SuiteScript`,
+    `Ruby`,
+    `SQL`,
+    `Lua`,
+    `Go`,
+    `Python`
+]
