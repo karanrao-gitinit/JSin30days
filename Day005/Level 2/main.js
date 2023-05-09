739d4b703e2f396d76e600b3cdb4e41e511f0a3b
Karan <karanrao81441@outlook.com>
2023-05-09 07:51:56 +0530

    Added solution for #4 of Level 2

--- Day005/Level 2/main.js
+++ Day005/Level 2/main.js
@@ -63,5 +63,10 @@ console.log(shoppingCart);
   #4 In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. 
   If it does not exist add to the countries list.
 */
-
+if (countries.includes(`Ethiopia`)) {
+  console.log(`ETHIOPIA`);
+} else {
+  countries.push(`Ethiopia`);
+  console.log(countries);
+}
 
