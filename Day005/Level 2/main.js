2b99384a862af382176aa6598b82d908d97f6208
Karan <karanrao81441@outlook.com>
2023-05-09 07:56:30 +0530

    Added solution for #5 of Level 2

--- Day005/Level 2/main.js
+++ Day005/Level 2/main.js
@@ -70,3 +70,13 @@ if (countries.includes(`Ethiopia`)) {
   console.log(countries);
 }
 
+/**
+ #5 In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+*/
+
+if (web_tech.includes(`Sass`)) {
+  console.log(`Sass is a CSS preprocess`);
+} else {
+  web_tech.unshift(`Sass`);
+  console.log(web_tech);
+}
