5bccf96465b3c034a80eabf1b9bea930bdafbabc
karanrao-gitinit <karanrao81441@outlook.com>
2023-05-15 23:58:24 +0530

    Added File for Day 6 and added solution for #1 of Level 1

---
+++ Day006/level1.js
@@ -0,0 +1,23 @@
+// #1 Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+// NOTE: Run both the codes separately
+
+// Loop
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// While loop
+let i = 0
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+// do-while loop
+do {
+    console.log(i);
+    i++
+} while (i <= 10);
+
+
