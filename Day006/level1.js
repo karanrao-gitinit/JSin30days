7aea50c7c56281590a429cf3c08e554ba6118f7a
karanrao-gitinit <karanrao81441@outlook.com>
2023-05-16 00:08:48 +0530

    Added solution for #2 of Level 2

--- Day006/level1.js
+++ Day006/level1.js
@@ -21,3 +21,23 @@ do {
 } while (i <= 10);
 
 
+// #2 Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+// Loop
+for (let x = 10; x >= 0; x--) {
+    console.log(x);
+}
+
+// while
+let x = 10;
+
+while (x >= 0) {
+    console.log(x);
+    x--;
+}
+
+// do-while loop
+do {
+    console.log(x);
+    x--;
+} while (x >= 10); 
