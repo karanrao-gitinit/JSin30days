dc74af2e8534b0f61d03a88304c77b7c340a6340
karanrao-gitinit <karanrao81441@outlook.com>
2023-05-23 08:43:01 +0530

    Added solution for #4 question of Level 2

--- Day006/Level 2/Questions 4 to 10/level2.js
+++ Day006/Level 2/Questions 4 to 10/level2.js
@@ -1,2 +1,20 @@
+/**
+    #4 Using the above countries array, create the following new array.
+    
+    ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
+*/
 const countries = require(`./countries`);
 
+const countriesPresent = [];
+
+const sampleCountries = ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"];
+
+for (let i = 0; i < countries.length; i++) {
+    let county = countries[i];
+
+    if (sampleCountries.includes(county.toUpperCase())) {
+        countriesPresent.push(county.toUpperCase());
+    }
+}
+
+console.log(countriesPresent);
