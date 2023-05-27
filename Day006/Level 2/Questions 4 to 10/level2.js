c67443a4e6d4cc132942adf4f7e2082d78547c86
karanrao-gitinit <karanrao81441@outlook.com>
2023-05-27 21:32:28 +0530

    Added solution for #6 question of Level 2

--- Day006/Level 2/Questions 4 to 10/level2.js
+++ Day006/Level 2/Questions 4 to 10/level2.js
@@ -32,3 +32,34 @@ for (let i = 0; i < sampleCountries.length; i++) {
 }
 
 console.log(sampleCountriesLenght);
+
+/**
+ #6 Use the countries array to create the following array of arrays:
+ 
+   [
+  ['Albania', 'ALB', 7],
+  ['Bolivia', 'BOL', 7],
+  ['Canada', 'CAN', 6],
+  ['Denmark', 'DEN', 7],
+  ['Ethiopia', 'ETH', 8],
+  ['Finland', 'FIN', 7],
+  ['Germany', 'GER', 7],
+  ['Hungary', 'HUN', 7],
+  ['Ireland', 'IRE', 7],
+  ['Iceland', 'ICE', 7],
+  ['Japan', 'JAP', 5],
+  ['Kenya', 'KEN', 5]
+    ]
+
+ */
+
+let arrayofCountries = [];
+
+for (let i = 0; i < sampleCountries.length; i++) {
+    
+    arrayofCountries.push(sampleCountries[i]);
+    arrayofCountries.push(sampleCountries[i].slice(0, 3));
+    arrayofCountries.push(sampleCountries[i].length);
+}
+
+console.log(arrayofCountries);
