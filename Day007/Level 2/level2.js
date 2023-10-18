53b2b70a1c588f302aa2e3e326c570ccdf785fd5
karanrao-gitinit <karanrao81441@outlook.com>
2023-10-19 01:22:31 +0530

    Added solution for Level 2 Quection #1

---
+++ Day007/Level 2/level2.js
@@ -0,0 +1,10 @@
+/*
+    Linear equation is calculated as follows: ax + by + c = 0.
+    Write a function which calculates value of a linear equation, solveLinEquation.
+*/
+
+function solveLinEquation(a, b, c) {
+    return (a + b + c)
+}
+
+console.log(solveLinEquation(10, 10, 10));
