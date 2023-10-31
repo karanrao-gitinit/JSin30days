cd617ee1a00c16426500b13e8df9d3fd196ef869
karanrao-gitinit <karanrao81441@outlook.com>
2023-10-31 08:25:12 +0530

    Added solution for #3 of Level 1 | Day #8

--- Day008/Level 1/day8_level1.js
+++ Day008/Level 1/day8_level1.js
@@ -6,8 +6,21 @@
     5. Set new properties the dog object: breed, getDogInfo
 */
 
-// 1.Empty object
+// #1 Empty object
 let dog = {}; 
 
-// 2. Print
-console.log(dog);
+// #2 Print
+console.log(dog); // output: {}
+
+// #3 Add Properties to the object
+dog.name = 'Fang'
+dog.legs = 4
+dog.colour = 'Grey'
+dog.age = 2
+dog.bark = 'woof woof'
+
+dog.getbark = function() {
+    return `Dog sounds like ${this.bark}`
+}
+
+console.log(dog.getbark()); // Output: Dog sounds like woof woof
